de999502af9dc28ddf33b868bb17581d234b309a
Balavardhan Reddy Malyala <62328664+balavardhan224@users.noreply.github.com>
2023-01-24 17:23:33 +0530

    Create reversearray.java

---
+++ reversearray.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class reverse{
+    public static void main(String[] args) {
+        int a[]={9,8,7,6,8};
+        System.out.println(a.length+"length");
+        for(int i=a.length-1;i>-1;i--){
+            System.out.println(a[i]+" ");
+        }
+        
+    }
+}
