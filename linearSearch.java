7789437553f12cb9d580bcfd7f232a46ed738ef1
Balavardhan Reddy Malyala <62328664+balavardhan224@users.noreply.github.com>
2023-01-24 19:01:44 +0530

    Create linearSearch.java

---
+++ linearSearch.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class lS{
+    static int search(int array[],int n){
+        for(int i=0;i<array.length;i++){
+            if(n==array[i]){
+                System.out.println("number located in "+i+"th index");
+               
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        int array[]={1,2,3,4,5};
+        System.out.println("search element");
+        int n=a.nextInt();
+        int p=search(array,n);
+        if (p==-1){
+            System.out.println("not found");
+        }
+        // else{
+        //     System.out.println("NOT FOUND");
+        // }
+       
+
+    }
+}
