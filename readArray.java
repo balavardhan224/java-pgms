47fcccf51a8235372af7b449b7609a1451518470
Balavardhan Reddy Malyala <62328664+balavardhan224@users.noreply.github.com>
2023-01-24 17:04:54 +0530

    Create readArray.java

---
+++ readArray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class bala{
+    public static void main(String[] args) {
+        Scanner a=new Scanner(System.in);
+        int[] array= new int[10];
+        for(int i=0;i<5;i++){
+        }
+        System.out.println("inserted");
+        for(int i=0;i<5;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
