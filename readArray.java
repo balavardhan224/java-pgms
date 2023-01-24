591663f75306e7f68512f4d29e2d872c84e3e960
Balavardhan Reddy Malyala <62328664+balavardhan224@users.noreply.github.com>
2023-01-24 17:04:59 +0530

    Update readArray.java

--- readArray.java
+++ readArray.java
@@ -4,6 +4,7 @@ class bala{
         Scanner a=new Scanner(System.in);
         int[] array= new int[10];
         for(int i=0;i<5;i++){
+            array[i]=a.nextInt();
         }
         System.out.println("inserted");
         for(int i=0;i<5;i++){
