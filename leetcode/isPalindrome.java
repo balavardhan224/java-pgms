34f996626bd629b05c4cb69d8e0eb727ba1328a9
Balavardhan Reddy Malyala <62328664+balavardhan224@users.noreply.github.com>
2023-02-03 03:37:19 +0530

    .

--- leetcode/isPalindrome.java
+++ leetcode/isPalindrome.java
@@ -1,4 +1,4 @@
-package leetcode;
+// package leetcode;
 
 class Solution1 {
     public static void main(String[] args) {
--- leetcode/romanToInt.java
+++ leetcode/romanToInt.java
@@ -1,5 +1,65 @@
+import java.util.*;
+
 // class Solution {
 //     public int romanToInt(String s) {
 //         String roman["I","V","X","L","C","D","M"]=[1,5,10,50,100,500,1000];
 //     }
 // }
+
+class romanToInt{
+    public static void main(String[] args) {
+        String a="IV";
+        
+        System.out.println(a);
+        int res=0;
+        for(int i=0;i<a.length();i++){
+            char c=a.charAt(i);
+            if(c=='I'){
+                if(
+                    (i+1<a.length() && (a.charAt(i+1)=='V' || a.charAt(i+1)=='X') )) {
+                   res-=1;
+                }
+                else{
+                    res+=1;
+                }
+            }
+            if(c=='V'){
+                if(
+                    (i+1<a.length() && (a.charAt(i+1)=='X' || a.charAt(i+1)=='L') )) {
+                   res-=5;
+                }
+                else{
+                    res+=5;}}
+            if(c=='X'){
+                if(
+                    (i+1<a.length() && (a.charAt(i+1)=='L' || a.charAt(i+1)=='C') )) {
+                   res-=10;
+                }
+                else{
+                    res+=10;}}
+            if(c=='L'){
+                if(
+                    (i+1<a.length() && (a.charAt(i+1)=='C' || a.charAt(i+1)=='D') )) {
+                   res-=50;
+                }
+                else{
+                    res+=50;}}
+            if(c=='C'){
+                if(
+                    (i+1<a.length() && (a.charAt(i+1)=='D' || a.charAt(i+1)=='M') )) {
+                   res-=100;
+                }
+                else{
+                    res+=100;}}
+            if(c=='D'){
+                res=res+500;}
+                if(c=='M'){
+                    res=res+1000;
+                }
+              
+        
+        
+        
+    }
+}
+}
